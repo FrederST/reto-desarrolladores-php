bb30525c6dbc08cd651ba13359914d13a6704206
FrederST <freder.hernandez@correounivalle.edu.co>
2022-01-08 23:58:18 -0500

    buil: New shop config for payment methods

--- config/shop.php
+++ config/shop.php
@@ -3,5 +3,13 @@
 return [
 
     'default_currency' => env('DEFAULT_CURRENCY_SHOP', 'USD'),
+    'default_payment_method' => env('DEFAULT_PAYMENT_METHOD', 'place_to_pay'),
+    'payment_methods' => [
+        'place_to_pay' => [
+            'api_url' => env('PLACE_TO_PAY_API_URL', 'https://dev.placetopay.com/redirection/'),
+            'login' => env('PLACE_TO_PAY_LOGIN', null),
+            'secretkey' => env('PLACE_TO_PAY_SECRET_KEY', null),
+        ],
+    ],
 
 ];

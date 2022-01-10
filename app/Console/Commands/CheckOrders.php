cddb6177b3fedc917fe02f11200c4a0672ae6a21
FrederST <freder.hernandez@correounivalle.edu.co>
2022-01-09 20:20:36 -0500

    feat: Chek orders command implementation

---
+++ app/Console/Commands/CheckOrders.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Actions\Order\CheckOrderAction;
+use App\Constants\OrderStatus;
+use App\Models\Order;
+use Illuminate\Console\Command;
+
+class CheckOrders extends Command
+{
+    protected $signature = 'check:orders';
+
+    protected $description = 'Command description';
+
+    public function handle(CheckOrderAction $checkOrderAction)
+    {
+        $ordersPending = Order::where('status', OrderStatus::STATUS_PENDING)
+        ->whereRaw('TIMESTAMPDIFF(MINUTE, created_at, "' . now() . '") > 7')->get();
+
+        foreach ($ordersPending as $item) {
+            $order = $checkOrderAction->execute($item);
+            $this->info('Checked Order '. $order->id);
+        }
+    }
+}

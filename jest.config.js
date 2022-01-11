164bfaffcd46f4c1995b34c8ad6af56564bbc154
FrederST <freder.hernandez@correounivalle.edu.co>
2022-01-10 20:53:24 -0500

    feat: Coverage report for jest

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,13 @@ module.exports = {
     globals: {},
     testEnvironment: 'jsdom',
     transform: {
-        "^.+\\.js$": "babel-jest",
-        ".*\\.(vue)$": "@vue/vue3-jest"
+        '^.+\\.js$': 'babel-jest',
+        '.*\\.(vue)$': '@vue/vue3-jest'
     },
     moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
     moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/resources/js/$1",
+        '^@/(.*)$': '<rootDir>/resources/js/$1',
     },
+    collectCoverage: true,
+    coverageReporters: ['json', 'html', 'lcov']
 }
